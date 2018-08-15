6d47fdb6d0212d7e9d24a2f6a38ec86cdaa26a31
kazusa4418 <astarte.rododaktylos@gmail.com>
2018-08-16 01:31:56 +0900

    Booth.java BoothManager.java 作成

---
+++ src/Booth.java
@@ -0,0 +1,46 @@
+public class Booth {
+	//部屋のステータス
+	private Status status;
+	//部屋番号
+	private final String boothNo;
+
+	Booth(Status status, String boothNo){
+		this.status = status;
+		this.boothNo = boothNo;
+	}
+
+	//部屋が使うことができるのか
+	public boolean isUnused(){
+		return status == Status.UNUSED;
+	}
+
+	//ステータスの更新時
+	public void setStatus(Status status){
+		this.status = status;
+	}
+
+	public String getBoothNo() {
+	    return boothNo;
+    }
+
+    public String toString() {
+	    return boothNo + " " + status;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
