9f9c65f7f2268d94bcdac9812e8d0a34737dac54
kazusa4418 <astarte.rododaktylos@gmail.com>
2018-09-06 19:26:19 +0900

    [fix] 有効性確認の返り値をステータスコードに変更したことに伴う修正

--- src/LoginUtils.java
+++ src/LoginUtils.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 public class LoginUtils {
     private static final String SELECT_SENTENCE = "SELECT * FROM user WHERE user_id = $user_id and password = $password";
 
-    public static boolean isEnabledUser(String userId, String password) throws FailedDatabaseAccessException {
+    public static LoginStatus isEnabledUser(String userId, String password) throws FailedDatabaseAccessException {
         try (MySql mysql = new MySql()) {
             try {
                 ResultSet resultSet = mysql.executeQuery(SELECT_SENTENCE.replace("$user_id", userId)
@@ -16,13 +16,14 @@ public class LoginUtils {
 
                 // SELECT文で取得できたレコードが0行だったらそのIDとPASSWORDを持つユーザーはいない
                 if (!resultSet.next()) {
-                    return false;
+                    return LoginStatus.UNMATCHED;
                 }
 
                 // ユーザーの有効期限を照会する
                 Date goodThrough = resultSet.getDate("good_through");
                 // 有効期限が切れていなければそのユーザーは有効
-                return goodThrough.toLocalDate().isAfter(LocalDate.now());
+                boolean isExpired = goodThrough.toLocalDate().isAfter(LocalDate.now());
+                return isExpired ? LoginStatus.EXPIRED : LoginStatus.ENABLED;
             }
             catch (SQLException err) {
                 throw new FailedDatabaseAccessException(2, "SELECT文の実行に失敗しました");
