cfa4e226ae668fbc389d55035fe9e3ec9db31830
kazusa4418 <astarte.rododaktylos@gmail.com>
2018-09-06 18:42:39 +0900

    [add] mysqlユーティリティの追加

---
+++ src/mysql/FailedDatabaseAccessException.java
@@ -0,0 +1,14 @@
+package mysql;
+
+class FailedDatabaseAccessException extends Exception {
+    private int errorId;
+
+    FailedDatabaseAccessException(int errorId, String errMsg) {
+        super(errMsg);
+        this.errorId = errorId;
+    }
+
+    int getErrorId() {
+        return errorId;
+    }
+}
