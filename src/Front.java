8a8dfac752756070105cffa8aefd1a3b1179ab8c
kazusa4418 <astarte.rododaktylos@gmail.com>
2018-09-06 20:53:09 +0900

    [update]機能追加

--- src/Front.java
+++ src/Front.java
@@ -1,2 +1,39 @@
+import java.util.*;
+
 public class Front {
+    public static void main(String[] args) {
+        //BoothManagerの起動
+        BoothManager boothManager = BoothManager.getInstance();
+        Scanner sc = new Scanner(System.in);
+        //会員情報の受付
+        System.out.println("会員番号を入力してださい");
+        int customerId = Integer.parseInt(sc.nextLine());
+        System.out.println("パスワードを入力してください");
+        int customerPass = Integer.parseInt(sc.nextLine());
+        Front.Login(customerId, customerPass);
+        //空室状況の問い合わせ
+
+        System.out.println("お部屋を選択してください");
+        String roomNum = sc.nextLine();
+        boothManager.setStatus(roomNum, Status.USED);
+        System.out.println("お客様のお部屋は" + roomNum + "になります");
+    }
+
+    boolean Login(int customerId, int customerPass) {
+
+        try {
+            boolean isEnabled = LoginUtils.isEnabledUser(customerId, customerPass);
+            if (isEnabled) {
+                System.out.println("お客様情報が確認できました。");
+            } else {
+                //LoginStatusにより分岐する
+                if()
+            }
+        } catch (FailedDatabaseAccessException e) {
+            System.out.println();
+        }
+
+        return loginStatus;
+    }
+
 }
