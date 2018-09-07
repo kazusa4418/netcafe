56c4c0279d90b46a7b2342f8e70b0c0046bca46a
Itkenta <k017c1311@it-neec.jp>
2018-09-07 23:13:05 +0900

    [add] 精算管理クラスの実装

--- src/CashRegister.java
+++ src/CashRegister.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.util.concurrent.TimeUnit;
 
 public class CashRegister {
+
+    private static long getUsedTime(Booth usedBooth){
+        //指定された部屋のお客様を受け取る
+        Customer usedCustomer = usedBooth.getCustomer();
+
+        return TimeUnit.MILLISECONDS.toSeconds(usedCustomer.getEnteredTime().getTime() - usedCustomer.getExitTime().getTime());
+    }
 }
+
+
