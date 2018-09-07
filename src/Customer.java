b196d865f89922415ffaac11c0a6c9aba0cc04dc
Itkenta <k017c1311@it-neec.jp>
2018-09-07 23:07:49 +0900

    [fix] 滞在時間の保持

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,18 @@
-public class Customer {
+import java.util.Date;
+
+class Customer {
+    private Date enteredTime;    //入店時刻
+
+    Customer() {
+        enteredTime = new Date();
+    }
+
+    Date getEnteredTime(){
+        return enteredTime;
+    }
+
+    Date getExitTime(){
+        return new Date();       //退店時刻
+    }
+
 }
