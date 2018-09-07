e586a89f2d87a969237c9350f965bf4d268ee167
Itkenta <k017c1311@it-neec.jp>
2018-09-07 12:19:12 +0900

    [update]スタブの作成

--- src/FailedDatabaseAccessException.java
+++ src/FailedDatabaseAccessException.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
 
-public class FailedDatabaseAccessException {
+public class FailedDatabaseAccessException extends Exception{
+    int err;
+    FailedDatabaseAccessException(int i, String s){
+        super(s);
+        this.err = i;
+    }
+
+    public int getErr(){
+        return err;
+    }
 }
