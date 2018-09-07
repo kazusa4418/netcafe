ffa7506f896c8bc505b66e32aff7c774d7f42142
Itkenta <k017c1311@it-neec.jp>
2018-09-07 23:09:38 +0900

    [fix] 入力されたパスワードの整合性確認の実装

--- src/Front.java
+++ src/Front.java
@@ -5,37 +5,64 @@ public class Front {
         //BoothManagerの起動
         BoothManager boothManager = BoothManager.getInstance();
         Scanner sc = new Scanner(System.in);
-        //会員情報の受付
+
+        //ログイン処理
         System.out.println("会員番号を入力してださい");
         int customerId = Integer.parseInt(sc.nextLine());
         System.out.println("パスワードを入力してください");
         int customerPass = Integer.parseInt(sc.nextLine());
-        Front.Login(customerId, customerPass);
-        //空室状況の問い合わせ
+        //不正なパスワードでないか検査する
 
-        System.out.println("お部屋を選択してください");
-        String roomNum = sc.nextLine();
-        boothManager.setStatus(roomNum, Status.USED);
-        System.out.println("お客様のお部屋は" + roomNum + "になります");
-    }
 
-    boolean Login(int customerId, int customerPass) {
+        isEnable(customerPass);
 
-        try {
-            boolean isEnabled = LoginUtils.isEnabledUser(customerId, customerPass);
-            if (isEnabled) {
-                System.out.println("お客様情報が確認できました。");
-            } else {
-                //磁気不良によりログインができなかった。
-                if(){
+        Login(customerId, customerPass);
 
-                }
-            }
-        } catch (FailedDatabaseAccessException e) {
-            System.out.println();
+        //空き部屋を表示するための処理
+        Booth unusedRooms [] = boothManager.getUnusedRooms();
+        for(Booth booth: unusedRooms){
+            System.out.print(booth.getBoothNo() + " ");
         }
 
-        return loginStatus;
+        //空室状況の問い合わせ
+        System.out.println("お部屋を選択してください");
+        String boothNum = sc.nextLine();
+
+        //部屋の割当の確定処理
+        boothManager.setStatus(boothNum);
+        System.out.println("お客様のお部屋は" + boothNum + "になります");
     }
 
+    private static boolean isEnable(int customerPass){
+        if (String.valueOf(customerPass).length() >= 30){
+         System.out.println("不正な値が入力されました");
+
+        }
+
+        return true;
+    }
+
+    private static void Login(int customerId, int customerPass) {
+        try {
+             LoginStatus isEnabled = LoginUtils.isEnabledUser(customerId, customerPass);
+             if (isEnabled.equals(LoginStatus.ENABLED)){
+                 System.out.println("お客様情報の確認がとれました");
+             }else {
+                 checkErrorStatus(isEnabled);
+                 System.exit(0);
+             }
+        } catch (FailedDatabaseAccessException e) {
+
+        }
+    }
+
+    private static void checkErrorStatus(LoginStatus errorLog){
+        if (errorLog.equals(LoginStatus.EXPIRED)){
+            System.out.println("お客様情報の有効期限が切れております");
+            System.out.println("従業員をお呼びのうえ、お客様情報の更新をお願いします");
+        }else{
+            System.out.println("お客様情報が正しく読み込めませんでした");
+            System.out.println("大変恐れ入りますが、最初の画面からもう一度お願いします");
+        }
+    }
 }
